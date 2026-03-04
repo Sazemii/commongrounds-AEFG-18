fb54891425b55d5808a80c425f391876e8dc87e9
Dexter Jethro Enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-03-05 00:36:59 +0800

    Added filters and customized display options for the admin panel

--- commongrounds/localevents/admin.py
+++ commongrounds/localevents/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 from .models import Event, EventType
 
 
+class EventInline(admin.TabularInline):
+    model = Event
+
+
 class EventTypeAdmin(admin.ModelAdmin):
     model = EventType
+    search_fields = ['name',]
+    list_display = ['name', 'description']
+    inlines = [EventInline,]
 
 
 class EventAdmin(admin.ModelAdmin):
     model = Event
+    search_fields = ['title', 'description', 'location']
+    list_filter = ['category', 'start_time', 'end_time']
+    list_display = ['title', 'category', 'location',
+                    'start_time', 'end_time', 'created_on',
+                    'updated_on']
 
 
 admin.site.register(EventType, EventTypeAdmin)

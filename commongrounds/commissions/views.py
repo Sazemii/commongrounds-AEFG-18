cb7a1ec39ee35d3e0e919ddce3c9929b009e4bd8
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-03-06 04:41:17 +0800

    Edited views.py with CommissionListView and CommissionDetailedView.

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Commission, CommisionType
+
+
+class CommissionListView(ListView):
+    model = Commission
+    template_name = 'commissions/commission_list.html'
+    context_object_name = 'commissions'
+    pass
+
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = 'commissions/commission_detail.html'
+    context_object_name = 'commission'
+    pass

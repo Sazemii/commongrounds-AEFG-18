c5817a7386073365ccc8d9a6c55edeee8e3e3a8b
Joselito Y. Cortes <joselitoycortes@Joselitos-iMac.local>
2026-03-05 22:54:53 +0800

    Created models and html pages

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name 
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        PostCategory,
+        on_delete=models.NULL,
+        null=True,
+    )
+    description = models.TextField()
+    Materials = models.TextField()
+    Steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('diyprojects:project_detail', args=[str(self.pk)])
+
 
 # Create your models here.

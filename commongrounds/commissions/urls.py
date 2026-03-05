24a7ac4ce76da20d94294f85c576560cee038ed9
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-03-06 04:54:02 +0800

    Added URL patterns

---
+++ commongrounds/commissions/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import CommissionListView, CommissionDetailView
+
+urlpatterns = [
+    path('commissions/requests',
+         CommissionListView.as_view(),
+         name='commission_list'),
+    path('commissions/request/<int:pk>',
+         CommissionDetailView.as_view(),
+         name='commission_detail'
+         )
+    
+]
+
+app_name = 'commissions'

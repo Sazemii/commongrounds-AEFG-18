637f90bd1ecaeda16edc5b5eea629286b72223e1
ThunderRabbit <zuzazibubabi@gmail.com>
2026-03-05 13:30:05 +0800

    added the required model specs

---
+++ commongrounds/bookclub/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    # for readability in shell/panel
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publication_year']
+
+    def __str__(self):
+        return self.title

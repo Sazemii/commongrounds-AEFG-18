a356e0f456a0048936ca5293cea9b15271ae441a
Dexter Jethro Enriquez <dexterjethro.enriquez@gmail.com>
2026-03-06 20:20:23 +0800

    Changed URL patterns for localevents to remove redundant prefixes

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('localevents.urls', namespace='localevents')),
+    path('localevents/', include('localevents.urls', namespace='localevents')),
 ]
--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import LocalEventsListView, LocalEventsDetailView
 
 urlpatterns = [
-    path('localevents/events',
+    path('events',
          LocalEventsListView.as_view(),
          name='event_list'),
-    path('localevents/event/<int:pk>',
+    path('event/<int:pk>',
          LocalEventsDetailView.as_view(),
          name='event_detail')
 ]

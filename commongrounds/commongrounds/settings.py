782cb626b6ad7b8be69f76bbce828767a329c3da
ThunderRabbit <zuzazibubabi@gmail.com>
2026-05-02 11:28:56 +0800

    setup: add homepage app, finals settings, auth URL wiring

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'diyprojects',
     'merchstore',
     'localevents',
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'

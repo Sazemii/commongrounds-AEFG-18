e5b58933d21a8f71a89e2509f13963244aec73db
Dexter Jethro Enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-05-04 12:19:51 +0800

    Added URL routing for localevents and adjusted list and detail views

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+# , LocalEventsCreateView, LocalEventsUpdateView, LocalEventsSignupView
 from .views import LocalEventsListView, LocalEventsDetailView
 
 urlpatterns = [
@@ -7,7 +8,16 @@ urlpatterns = [
          name='event_list'),
     path('event/<int:pk>',
          LocalEventsDetailView.as_view(),
-         name='event_detail')
+         name='event_detail'),
+    #     path('event/add',
+    #          LocalEventsCreateView.as_view(),
+    #          name='event_add'),
+    #     path('event/<int:pk>/edit',
+    #          LocalEventsUpdateView.as_view(),
+    #          name='event_edit'),
+    #     path('event/<int:pk>/signup',
+    #          LocalEventsSignupView.as_view(),
+    #          name='event_signup'),
 ]
 
 app_name = 'localevents'
--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -2,18 +2,48 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Event, EventType
+from .models import Event, EventType, EventSignup
 
 
 class LocalEventsListView(ListView):
     model = Event
     template_name = 'localevents/event_list.html'
     context_object_name = 'events'
-    pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_authenticated:
+            profile = user.profile
+            context['organized_events'] = Event.objects.filter(
+                organizer=profile)
+            context['signed_up_events'] = Event.objects.filter(
+                signups__user_registrant=profile
+            )
+            context['events'] = Event.objects.exclude(
+                pk__in=context['organized_events'],
+            ).exclude(
+                pk__in=context['signed_up_events'],
+            )
+        return context
 
 
 class LocalEventsDetailView(DetailView):
     model = Event
     template_name = 'localevents/event_detail.html'
     context_object_name = 'event'
-    pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        event = self.object
+        user = self.request.user
+        context['is_full'] = event.signups.count() >= event.event_capacity
+
+        if user.is_authenticated:
+            profile = user.profile
+            context['is_organizer'] = profile in event.organizer.all()
+        else:
+            context['is_organizer'] = False
+
+        return context
